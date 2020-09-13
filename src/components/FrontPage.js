7179d4b84a30e365aea60b31c2dde6933c2fec8e
Ahsan <innocent.ahsan12345@gmail.com>
2020-09-13 23:07:48 +0500

    adding parallax in home and Re-decorate them

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,21 +1,55 @@
 import React from 'react'
-import frontPic from '../../public/images/frontPic.jpg'
+import creative1 from '../../public/images/creative1.jpg'
+import { Parallax } from 'react-parallax'
+
 
 const FrontPage = () => {
     
     return (
         <div className='frontpage'>
             <div className='frontpage__media'>
-                <img  src={frontPic} />
+            
+            <Parallax bgImage={creative1} strength={700}>
+                {/* <div className='para'>
+                    <h5>parallel</h5>
+                </div> */}
+                <div className='frontpage__text'>
+                   <div className='name_text'>
+                        <h2>I'm</h2> <h1>
+                            <span className='a'>A</span>
+                            <span className='h'>h</span>
+                            <span className='s'>s</span>
+                            <span className='e'>a</span>
+                            <span className='n'>n</span>
+                        </h1>
+                   </div>
+                     <div className='animate__line'>
+                        And I'm specialized in Front-end-web-development <span>React-Apps and AI engineering</span>
+                     </div>
+                </div>
+            </Parallax>
                 
 
-                <div className='frontpage__text'>
-                     <span style={{ marginLeft: '110px' }}>I'am</span> <span style={{ marginLeft: '20px'}}>Ahsan</span>
+
+
+            
+                {/* <div className='frontpage__text'>
+                   <div className='ll'>
+                        I'am <h1>
+                            <span className='a'>A</span>
+                            <span className='h'>h</span>
+                            <span className='s'>s</span>
+                            <span className='e'>a</span>
+                            <span className='n'>n</span>
+                        </h1>
+                   </div>
                      <div className='animate__line'>
                         And Im developing <span></span>
                      </div>
-                </div>
+                </div> */}
         
+                
+
              </div>
         </div>
     )
